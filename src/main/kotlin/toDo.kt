fd5fe11cb1bfce1eea8d70fec850d69ddb747926
jaychy-yy <0816jinjin@gmail.com>
2021-07-03 22:53:24 +0900

    feat: ToDo 컴포넌트 생성.

--- src/main/kotlin/toDo.kt
+++ src/main/kotlin/toDo.kt
@@ -1,2 +1,32 @@
-class toDo {
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.a
+import react.dom.button
+import styled.css
+import styled.styledDiv
+
+external interface ToDoProps : RProps {
+    val message: String
+}
+
+@JsExport
+class ToDo : RComponent<ToDoProps, RState>() {
+    override fun RBuilder.render() {
+        styledDiv {
+            css {
+
+            }
+            a {
+                +props.message
+            }
+            button {
+                
+            }
+            button {
+
+            }
+        }
+    }
 }
