eb8b889d1bb65d55d9b54d8435c53c968a8793bd
jaychy-yy <0816jinjin@gmail.com>
2021-07-03 22:52:10 +0900

    feat: ToDoList의 본문 정의.

--- src/main/kotlin/toDoList.kt
+++ src/main/kotlin/toDoList.kt
@@ -1,2 +1,25 @@
-class ToDoList {
+import react.*
+import styled.css
+import styled.styledDiv
+
+@JsExport
+class ToDoList: RComponent<RProps, RState>() {
+    override fun RBuilder.render() {
+        styledDiv {
+            css {
+                +MainStyles.todoListBody
+            }
+            +"나다"
+        }
+    }
+}
+
+//fun RBuilder.toDoList(handler: RProps.() -> Unit): ReactElement {
+//    return child(ToDoList::class) {
+//        this.attrs(handler)
+//    }
+//}
+
+fun RBuilder.toDoList(): ReactElement {
+    return child(ToDoList::class) {}
 }
